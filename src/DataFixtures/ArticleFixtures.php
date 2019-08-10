bccbc5bbd03d5eb73565084cfca3ef55ec807a72
Cyril Vassallo <cyrilvssll34@gmail.com>
2019-08-10 10:07:10 +0200

    first commit

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Article;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for($i= 0 ; $i < 5; $i++){
+           $article = new Article(); 
+           $article->setTitle('Titre de l\'article N '. $i)
+                    ->setContent('<p>Contenu de l\'article N '.$i.'</p>')
+                    ->setImage('http://www.placehold.it/350x150')
+                    ->setCreatedAt(new \DateTime());
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+}
+
